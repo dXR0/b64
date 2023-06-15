9734513ac42535932418988b432007ed43033d6e
dXR0 <meelis.utt+dXR0@gmail.com>
2023-06-15 22:37:37 +0200

    implement decode

--- b64.c
+++ b64.c
@@ -6,8 +6,7 @@
 #define MAX_STR 256
 #define B64T "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
-// TODO: cleanup funcs and naming
-// TODO: implement decoding
+// TODO: cleanup funcs and naming/refactor
 
 // borrowed from tsoding
 const char *shift(int *argc, char ***argv)
@@ -19,12 +18,12 @@ const char *shift(int *argc, char ***argv)
     return result;
 }
 
-int my_strcmp(const char *s1, const char *s2)
+int my_strcmp(const char *base, const char *comp)
 {
 	int i;
-	for (i=0; s1[i] == s2[i]; ++i)
-		if (s1[i] == '\0') return 0;
-	return s1[i] - s2[i];
+	for (i=0; base[i] == comp[i]; ++i)
+		if (base[i] == '\0') return 0;
+	return base[i] - comp[i];
 }
 
 int my_strlen(const char *s)
@@ -34,7 +33,6 @@ int my_strlen(const char *s)
 		++i;
 	return i;
 }
-
 
 void mseti(int *dst, int v, size_t size)
 {
@@ -50,19 +48,13 @@ void msetc(char *dst, int v, size_t size)
 	}
 }
 
-void print_binaryi(int *bs, size_t size)
+void print_binary(int *bs, size_t size)
 {
 	for (int i=0; i<size; ++i) printf("%d", bs[i]);
 	putchar('\n');
 }
 
-void print_binaryc(char *bs, size_t size)
-{
-	for (int i=0; i<size; ++i) printf("%c", bs[i]);
-	putchar('\n');
-}
-
-void stob(int bs[], char *ss, size_t ss_size)
+void estob(int bs[], char *ss, size_t ss_size)
 {
 	// first byte of 8 needs to be 0;
 	for (int i=0; i<ss_size; ++i) {
@@ -78,7 +70,7 @@ void stob(int bs[], char *ss, size_t ss_size)
 	}
 }
 
-void btos(char *ss, int bs[], size_t bs_size, size_t padding)
+void ebtos(char *ss, int bs[], size_t bs_size, size_t padding)
 {
 	int k = 0;
 	for (int i=0; i<bs_size; i+=6) {
@@ -93,7 +85,6 @@ void btos(char *ss, int bs[], size_t bs_size, size_t padding)
 	}
 	int kpadding = k+padding;
 	for (; k<kpadding; ++k) ss[k] = '=';
-	
 }
 
 void encode(char *buf, int ctr)
@@ -103,16 +94,78 @@ void encode(char *buf, int ctr)
 	bs_size += 2*padding;
 	int bs[bs_size];
 	mseti(bs, 0, bs_size);
-	// print_binaryi(bs, bs_size);
-	stob(bs, buf, ctr);
-	// print_binaryi(bs, bs_size);
+	// print_binary(bs, bs_size);
+	estob(bs, buf, ctr);
+	// print_binary(bs, bs_size);
 	size_t b64_size = bs_size/6 + padding + (padding > 0) + 1; // non-zero padding means extra char; +1 for '\0'
 	char b64[b64_size];
 	msetc(b64, '\0', b64_size);
-	btos(b64, bs, bs_size, padding);
-	// print_binaryc(b64, b64_size);
+	ebtos(b64, bs, bs_size, padding);
 	printf("%s\n", b64);
 }
+
+void dstob(int bs[], char *ss, size_t ss_size)
+{
+	for (int i=0; i<ss_size; ++i) {
+		// naive method: find the index of the letter
+		// TODO: implement faster searching algorithm
+		int idx = -1;
+		for (int j=0; j<64; ++j){
+			if (ss[i] == B64T[j]) {
+				idx = j;
+				break;
+			}
+		}
+		int ones_count = 0;
+		int j = 5; // it's filled in reverse order
+		while(idx >= 0 && ones_count < 1) {
+			if (idx <= 1) ++ones_count;
+			bs[6*i+j] = idx%2;
+			idx/=2;
+			--j;
+		} 
+	}
+}
+
+void dbtos(char *ss, int bs[], size_t bs_size)
+{
+	int k = 0;
+	for (int i=0; i<bs_size; i+=8) {
+		int res = bs[i+7];
+		int pow = 2;
+		for (int j=6; j>-1; --j) {
+			res += bs[i+j]*pow;
+			pow *= 2;
+		}
+		ss[k] = res;
+		++k;
+	}
+}
+
+size_t count_padding(char *buf, size_t ctr)
+{
+	int count = 0;
+	for (int i=0; i<ctr; ++i) {
+		if (buf[i] == '=') ++count;
+	}
+	return count;
+}	
+
+void decode(char *buf, int ctr)
+{
+	size_t padding = count_padding(buf, ctr);
+	size_t bs_size = (ctr-padding)*6 - (6-padding);
+	int bs[bs_size];
+	mseti(bs, 0, bs_size);
+	dstob(bs, buf, ctr);
+	// print_binary(bs, bs_size);
+	size_t ss_size = bs_size/8+(bs_size%8!=0)+1; // +1 for '\0'
+	char ss[ss_size];
+	msetc(ss,'\0', ss_size);
+	dbtos(ss, bs, bs_size);
+	printf("%s\n", ss);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -121,11 +174,12 @@ int main(int argc, char **argv)
 	if (argc > 0) {
 		action = shift(&argc, &argv);
 	}
-	const int is_encode = !my_strcmp(action, "encode");
-	const int is_decode = !my_strcmp(action, "decode");
+	const int is_encode = !my_strcmp("encode", action);
+	const int is_decode = !my_strcmp("decode", action);
 
 	if (!is_encode && !is_decode) {
 		printf("unknown action: %s\n", action);
+		printf("allowed: encode decode\n");
 		exit(1);
 	}
 	
@@ -136,7 +190,7 @@ int main(int argc, char **argv)
 		const char *arg = shift(&argc, &argv);
 		size_t arglen = my_strlen(arg);
 		if (is_encode) encode((char *)arg, arglen);
-		// if (is_decode) decode(arg, arglen);
+		else if (is_decode) decode((char *)arg, arglen);
 	}
 	if (from_cmd) exit(0);
 
@@ -157,6 +211,7 @@ int main(int argc, char **argv)
 				++ctr;
 			} else {
 				if (is_encode) encode(buf, ctr);
+				else if (is_decode) decode(buf, ctr);
 				break;
 			}
 		}
@@ -171,7 +226,7 @@ int main(int argc, char **argv)
 				++ctr;
 			} else {
 				if (is_encode) encode(buf, ctr);
-				// else if (is_decode ) decode(buf, ctr);
+				else if (is_decode ) decode(buf, ctr);
 				ctr=0;
 			}
 		}
@@ -182,6 +237,7 @@ int main(int argc, char **argv)
 		char buf[len];
 		fgets(buf, len, stream);
 		if (is_encode) encode(buf, len-1); // -1 len, because I guess of EOF
+		else if (is_decode) decode(buf, len-1); // -1 len, because I guess of EOF
 	}
 	return fclose(stream);
 }
