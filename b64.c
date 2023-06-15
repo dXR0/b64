057c5ea82eb971bdbc5932ab994c88860c477f90
dXR0 <meelis.utt+dXR0@gmail.com>
2023-06-15 18:40:15 +0200

    fixed b64 encoding for repl type

--- b64.c
+++ b64.c
@@ -6,30 +6,44 @@
 #define MAX_STR 256
 #define B64T "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 
-void mset(int *dst, int v, size_t size)
+void mseti(int *dst, int v, size_t size)
 {
 	for (int i=0; i < size; ++i) {
 		dst[i] = v;
 	}
 }
 
-void print_binary(int *bs, size_t size)
+void msetc(char *dst, int v, size_t size)
+{
+	for (int i=0; i < size; ++i) {
+		dst[i] = v;
+	}
+}
+
+void print_binaryi(int *bs, size_t size)
 {
 	for (int i=0; i<size; ++i) printf("%d", bs[i]);
-	printf("\n");
+	putchar('\n');
+}
+
+void print_binaryc(char *bs, size_t size)
+{
+	for (int i=0; i<size; ++i) printf("%c", bs[i]);
+	putchar('\n');
 }
 
 void stob(int bs[], char *ss, size_t ss_size)
 {
+	// first byte of 8 needs to be 0;
 	for (int i=0; i<ss_size; ++i) {
 		int ones_count = 0;
-		int j = 1; // first byte of 8 needs to be 0
+		int j = 7; // it's filled in reverse order
 		char c = ss[i];
-		while (c >= 0 && ones_count < 1) {
+		while(c >= 0 && ones_count < 1) {
 			if (c <= 1) ++ones_count;
 			bs[8*i+j] = c%2;
 			c/=2;
-			++j;
+			--j;
 		} 
 	}
 }
@@ -47,7 +61,29 @@ void btos(char *ss, int bs[], size_t bs_size, size_t padding)
 		ss[k] = B64T[res];
 		++k;
 	}
-	for (int i=0; i<padding; ++i) ss[k+i] = '=';
+	int kpadding = k+padding;
+	for (; k<kpadding; ++k) ss[k] = '=';
+	
+}
+
+void encode(char *buf, int ctr)
+{
+	size_t bs_size = ctr*8;
+	size_t padding = ((6 - (bs_size%6))%6)/2;
+	bs_size += 2*padding;
+	int bs[bs_size];
+	mseti(bs, 0, bs_size);
+	// print_binaryi(bs, bs_size);
+	stob(bs, buf, ctr);
+	// print_binaryi(bs, bs_size);
+	size_t b64_size = bs_size/6 + padding + (padding > 0) + 1; // non-zero padding means extra char; +1 for '\0'
+	char b64[b64_size];
+	msetc(b64, '\0', b64_size);
+	// printf("%s\n", b64);
+	// print_binaryc(b64, b64_size);
+	btos(b64, bs, bs_size, padding);
+	// print_binaryc(b64, b64_size);
+	printf("%s\n", b64);
 }
 
 int main(int argc, char **argv)
@@ -68,21 +104,8 @@ int main(int argc, char **argv)
 				buf[ctr] = c;
 				++ctr;
 			} else {
-				size_t bs_size = ctr*8;
-				size_t padding = ((6 - (bs_size%6))%6)/2;
-				bs_size += padding;
-				int bs[bs_size];
-				mset(bs, 0, bs_size+padding);
-				print_binary(bs, bs_size);
-				stob(bs, buf, ctr);
-				print_binary(bs, bs_size);
-				size_t b64_size = bs_size/6 + padding + (padding > 0); // non-zero padding means extra char
-				char b64[b64_size];
-				mset(b64, 0, b64_size);
-				print_binary(b64, b64_size);
-				btos(b64, bs, bs_size, padding);
-				printf("%s\n", b64);
-				ctr = 0;
+				encode(buf, ctr);
+				ctr=0;
 			}
 		}
 	} else { // file directed in as stdin, eg ./a.out < file
