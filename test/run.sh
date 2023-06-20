364f1fd3eb11e88de0d01441f9ec9afe80ba054e
dXR0 <meelis.utt+dXR0@gmail.com>
2023-06-20 21:31:59 +0200

    rm test/

--- test/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cc ../b64.c
-rm -rf testdata*
-rm -f xxx.test
-go test -c b64_test.go
-./xxx.test -test.fuzzcachedir=testdata_encode -test.fuzz=Encode -test.fuzztime=10s
-./xxx.test -test.fuzzcachedir=testdata_decode -test.fuzz=Decode -test.fuzztime=10s
-rm -f xxx.test a.out
